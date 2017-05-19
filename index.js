3f657e60c3ddea9a2680b56ebe2bd1220d39edb6
r00t3g <alexey.zarubin@gmail.com>
2017-05-19 11:37:54 +0300

    HBS Filename enclosure

--- index.js
+++ index.js
@@ -329,7 +329,7 @@ module.exports = function (source) {
             var slug = template ?
             'var Handlebars = require(' + JSON.stringify(runtimePath) + ');\n'
             + 'function __default(obj) { return obj && (obj.__esModule ? obj["default"] : obj); }\n'
-            + 'module.exports = (Handlebars["default"] || Handlebars).template(' + template + ');' :
+            + 'module.exports = (function($__hbsFileName){ return (Handlebars["default"] || Handlebars).template(' + template + '); })("'+resourcePath+'")' :
                 'module.exports = function(){return "";};';
 
             if (query.cache) {
